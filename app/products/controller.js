08d354bfc8e52328c364f5d5580f0a8ed981de73
Ahmad <ahmad.rpl001@gmail.com>
2021-02-21 01:47:35 +0700

    Upload Image Product

--- app/products/controller.js
+++ app/products/controller.js
@@ -1,11 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('./model');
+const config = require('../config');
 
 async function store(req, res, next) {
+
     try {
+
         let payload = req.body;
-        let product = new Product(payload);
-        await product.save();
-        return res.json(product)
+
+        if (req.file) {
+            let tmp_path = req.file.path;
+            let originalExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+            let filename = req.file.filename + '.' + originalExt;
+            let target_path = path.resolve(config.rootPath, `public/upload/${filename}`);
+
+            const src = fs.createReadStream(tmp_path);
+            const dest = fs.createWriteStream(target_path);
+            src.pipe(dest);
+
+            src.on('end', async () => {
+                let product = new Product({ ...payload, image_url: filename });
+                await product.save();
+                return res.json(product);
+            });
+
+            src.on('error', async () => {
+                next(err);
+            });
+
+        } else {
+            let product = new Product(payload);
+            await product.save();
+            return res.json(product);
+
+        }
     } catch (err) {
         if (err && err.name === 'ValidationError') {
             return res.json({
@@ -14,10 +44,9 @@ async function store(req, res, next) {
                 fields: err.errors
             });
         }
-        next(err)
+
+        next(err);
     }
-
-
 }
 
 module.exports = {
