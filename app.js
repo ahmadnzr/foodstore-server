db5e1a85b27271bcfc257a04a4f40ab2563301e0
Ahmad <ahmad.rpl001@gmail.com>
2021-02-23 08:38:20 +0700

    Membuat Endpoint Store Category

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const productRouter = require('./app/products/router')
+const productRouter = require('./app/products/router');
+const categoryController = require('./app/categories/router');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', productRouter)
+app.use('/api', categoryController)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
