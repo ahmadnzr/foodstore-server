40fff8b55efb8354a8c6fefd4fff3cada18f3564
Ahmad <ahmad.rpl001@gmail.com>
2021-02-23 08:48:38 +0700

    Membuat Endpoint Delete Category

--- app/categories/controller.js
+++ app/categories/controller.js
@@ -22,8 +22,8 @@ async function update(req, res, next) {
     try {
 
         let payload = req.body;
-        let category = 
-        await Category.findOneAndUpdate({ _id: req.params.id }, payload, { new: true, runValidators: true })
+        let category =
+            await Category.findOneAndUpdate({ _id: req.params.id }, payload, { new: true, runValidators: true })
         return res.json(category);
     } catch (err) {
 
@@ -38,7 +38,18 @@ async function update(req, res, next) {
         next(err);
     }
 }
+
+async function destroy(req, res, next) {
+    try {
+        let deleted = await Category.findOneAndDelete({ _id: req.params.id });
+        return res.json(deleted)
+    } catch (err) {
+        next(err);
+    }
+}
+
 module.exports = {
     store,
-    update
+    update,
+    destroy
 }
