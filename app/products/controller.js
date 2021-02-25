2fb02bf0fa40726d819fd087b1dfb6b0feca2fd5
Ahmad <ahmad.rpl001@gmail.com>
2021-02-25 08:24:37 +0700

    Filter product berdasarkan Keyword

--- app/products/controller.js
+++ app/products/controller.js
@@ -77,11 +77,18 @@ async function index(req, res, next) {
 
     try {
 
-        let { limit = 10, skip = 0 } = req.query;
+        let { limit = 10, skip = 0, q = '' } = req.query;
+        let criteria = {}
+        if (q.length) {
+            criteria = {
+                ...criteria,
+                name: { $regex: `${q}`, $options: 'i' }
+            }
+        }
 
         let products =
             await Product
-                .find()
+                .find(criteria)
                 .limit(parseInt(limit))
                 .skip(parseInt(skip))
                 .populate('category')
