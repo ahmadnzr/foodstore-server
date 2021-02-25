c0d3fa62b637e3edeb9f629af9669769baf12e2b
Ahmad <ahmad.rpl001@gmail.com>
2021-02-25 14:45:01 +0700

    Filtering berdasarkan tags

--- app/products/controller.js
+++ app/products/controller.js
@@ -77,7 +77,7 @@ async function index(req, res, next) {
 
     try {
 
-        let { limit = 10, skip = 0, q = '', category = '' } = req.query;
+        let { limit = 10, skip = 0, q = '', category = '', tags = [] } = req.query;
         let criteria = {}
 
         if (q.length) {
@@ -100,6 +100,11 @@ async function index(req, res, next) {
             if (category) {
                 criteria = { ...criteria, category: category._id }
             }
+        }
+
+        if (tags.length) {
+            tags = await Tag.find({name: {$in: tags}})
+            criteria = {...criteria, tags: {$in: tags.map(tag => tag._id)}}
         }
 
         let products =
