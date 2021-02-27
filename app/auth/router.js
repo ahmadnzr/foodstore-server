fff6e4c122fe3acf74f29fdde994f935afb6cfe4
Ahmad <ahmad.rpl001@gmail.com>
2021-02-28 01:50:44 +0700

    endpoint me

--- app/auth/router.js
+++ app/auth/router.js
@@ -8,5 +8,6 @@ const LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy({usernameField: 'email'},controller.localStrategy));
 router.post('/register', multer().none(),controller.register);
 router.post('/login', multer().none(), controller.login)
+router.get('/me', controller.me);
 
 module.exports = router
