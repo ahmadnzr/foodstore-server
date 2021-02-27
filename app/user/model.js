ca0b772c26699e4d7e09cdb30013b3c1bbfbc65d
Ahmad <ahmad.rpl001@gmail.com>
2021-02-28 00:39:00 +0700

    Validasi email dengan custom function

--- app/user/model.js
+++ app/user/model.js
@@ -29,4 +29,19 @@ let userSchema = Schema({
     token: [String]
 }, { timestamps = true });
 
+userSchema.path('email').validate(async function(value){
+    try{
+        const count = await this.model('User').count({email: value})
+        return !count;
+    }catch(err){
+        throw err
+    }
+}, attr => `${attr.value} sudah terdaftar`)
+
+userSchema.path('email').validate(function(value){
+    const emailRE = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailRE.test(value)
+}, attr => `${attr.value} harus merupakan email yang valid!`);
+
+
 module.exports = model('User', userSchema)
